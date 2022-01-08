82d5d7a3948869a0c0c540330fb18f53349439cd
goostavo25 <groogonzalez@my365.bellevue.edu>
2022-01-08 11:40:40 -0500

    Added ts-examples

---
+++ week-1/ts-examples/person.class.ts
@@ -0,0 +1,27 @@
+/*
+================================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: 08 January 2022
+; Modified By: Gustavo Roo
+; Description: Program to demonstrate Angular
+;===============================================
+*/
+
+// Import IPerson
+import { IPerson } from "./person.interface";
+
+// Create Person
+class Person implements IPerson {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+// Declare variable myName
+let myName = new Person("Gustavo", "Roo Gonzalez");
+console.log(`My name is ${myName.firstName} ${myName.lastName}`);
